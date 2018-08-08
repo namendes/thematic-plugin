629e59f84039b3f7d7ca29d53df98e1acfb65995
Varun <varun.wachaspati@bloomreach.com>
2018-08-08 15:09:49 +0530

    Clean Archetype for Demo

--- plugins/demo/repository-data/webfiles/src/main/resources/site/freemarker/myhippoproject/test.ftl
+++
@@ -1,8 +0,0 @@
-<#include "../include/imports.ftl">
-
-<h1>Thematic</h1>
-<#list news.getValue('response').getValue('docs').children.collection as newsDoc>
-
-<p>${newsDoc.getValue('description')}</p>
-
-</#list>
---
+++ plugins/dxpfc-demo/site/src/main/java/com/dxpfc/beans/BaseDocument.java
@@ -0,0 +1,9 @@
+package com.dxpfc.beans;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoDocument;
+
+@Node(jcrType="dxpfcdemo:basedocument")
+public class BaseDocument extends HippoDocument {
+
+}

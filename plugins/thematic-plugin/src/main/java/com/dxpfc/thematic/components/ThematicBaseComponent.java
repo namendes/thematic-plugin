645e0309d513ded614676949e7e8009c40246b20
Assim Ambadi Pottammal <assim.pottammal@bloomreach.com>
2018-08-09 12:05:14 +0530

    intermediate with yaml files and all

--- plugins/thematic-plugin/src/main/java/com/dxpfc/thematic/components/ThematicBaseComponent.java
+++ plugins/thematic-plugin/src/main/java/com/dxpfc/thematic/components/ThematicBaseComponent.java
@@ -14,8 +14,12 @@ public class ThematicBaseComponent extends BaseHstComponent {
   public void doBeforeRender(HstRequest request, HstResponse response) throws HstComponentException {
 
     ResourceServiceBroker broker = HippoServiceRegistry.getService(ResourceServiceBroker.class);
-    Resource thematic = broker.resolve("thematicAPI", "");
-
+    String path = "?rows=100&account_id=5017&domain_key=neimanmarcus_com&request_id=1&url=xx&fl=pid,title,brand,price,sale_price,promotions,thumb_image,sku_thumb_images,sku_swatch_images,sku_color_group,url,price_range,sale_price_range,description&sc2_mode=True&request_type=thematic&q=%s&debug=true&";
+    String baseUrl = request.getRequestContext().getBaseURL().getRequestPath();
+    String splitUrl[] = baseUrl.split("/");
+    int urlLength = splitUrl.length;
+    String theme = splitUrl[urlLength-1];
+    Resource thematic = broker.resolve("thematicAPI", String.format(path, theme));
     request.setAttribute("thematic", thematic);
   }
 }

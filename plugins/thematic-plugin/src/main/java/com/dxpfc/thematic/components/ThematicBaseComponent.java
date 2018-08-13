66f7254bfd2636be9ef82d209629db6898f13947
Assim Ambadi Pottammal <assim.pottammal@bloomreach.com>
2018-08-13 16:53:43 +0530

    after making configurable

--- plugins/thematic-plugin/src/main/java/com/dxpfc/thematic/components/ThematicBaseComponent.java
+++ plugins/thematic-plugin/src/main/java/com/dxpfc/thematic/components/ThematicBaseComponent.java
@@ -8,18 +8,36 @@ import org.onehippo.cms7.crisp.api.broker.ResourceServiceBroker;
 import org.onehippo.cms7.crisp.api.resource.Resource;
 import org.onehippo.cms7.services.HippoServiceRegistry;
 
+import java.util.Map;
+
 public class ThematicBaseComponent extends BaseHstComponent {
 
   @Override
   public void doBeforeRender(HstRequest request, HstResponse response) throws HstComponentException {
 
     ResourceServiceBroker broker = HippoServiceRegistry.getService(ResourceServiceBroker.class);
-    String path = "?rows=100&account_id=5017&domain_key=neimanmarcus_com&request_id=1&url=xx&fl=pid,title,brand,price,sale_price,promotions,thumb_image,sku_thumb_images,sku_swatch_images,sku_color_group,url,price_range,sale_price_range,description&sc2_mode=True&request_type=thematic&q=%s&debug=true&";
+    String path = "?rows=%s&account_id=%s&domain_key=%s&request_id=%s&url=%s&fl=%s&sc2_mode=%s&request_type=%s&q=%s&debug=%s&";
     String baseUrl = request.getRequestContext().getBaseURL().getRequestPath();
     String splitUrl[] = baseUrl.split("/");
     int urlLength = splitUrl.length;
     String theme = splitUrl[urlLength-1];
-    Resource thematic = broker.resolve("thematicResource", String.format(path, theme));
+    Map<String,String> properties = getComponentParameters();
+    String fl = properties.get("fields");
+    String row = properties.get("max_results_per_page");
+    String accountId = properties.get("account_id");
+    String domainKey = properties.get("domain_key");
+    String sc2Mode = properties.get("sc2_mode");
+    String requestType = properties.get("request_type");
+    String debugMode = properties.get("debug_mode");
+    String requestId = "1";
+    String url = "hippo:" + baseUrl;
+
+    if (requestType.equals("search")) {
+      requestType = "search&search_type=keyword";
+    }
+
+    Resource thematic = broker.resolve("thematicResource", String.format(path, row, accountId, domainKey, requestId, url, fl, sc2Mode, requestType, theme, debugMode));
     request.setAttribute("thematic", thematic);
+
   }
 }

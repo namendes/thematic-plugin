2a245b3e053f0f9d2b95bb56369291e18f8f9972
Varun <varun.wachaspati@bloomreach.com>
2018-09-20 15:44:40 +0530

    Added minimal control-panel

--- plugins/thematic-plugin/src/main/java/com/dxpfc/thematic/components/ThematicBaseComponent.java
+++ plugins/thematic-plugin/src/main/java/com/dxpfc/thematic/components/ThematicBaseComponent.java
@@ -20,8 +20,6 @@ public class ThematicBaseComponent extends BaseHstComponent {
   public void doBeforeRender(HstRequest request, HstResponse response) throws HstComponentException {
 
     // log.info("Thematic Request - "+ "http://"+request.getRequestContext().getBaseURL().getHostName()+request.getRequestContext().getBaseURL().getContextPath()+request.getRequestContext().getBaseURL().getRequestPath());
-    UUID uuid = UUID.randomUUID();
-    uuid.toString();
 
     ResourceServiceBroker broker = HippoServiceRegistry.getService(ResourceServiceBroker.class);
     String path = "?rows=%s&account_id=%s&domain_key=%s&request_id=%s&url=%s&fl=%s&sc2_mode=%s&request_type=%s&q=%s&debug=%s&";
@@ -37,7 +35,7 @@ public class ThematicBaseComponent extends BaseHstComponent {
     String sc2Mode = properties.get("sc2_mode");
     String feRequestType = properties.get("request_type");
     String debugMode = properties.get("debug_mode");
-    String requestId = "1";
+    String requestId = UUID.randomUUID().toString();
     String url = "hippo:" + baseUrl;
 
     if (feRequestType.equals("search")) {
@@ -46,7 +44,7 @@ public class ThematicBaseComponent extends BaseHstComponent {
 
     request.setAttribute("rsp",new Boolean(false));
    try {
-     Resource thematic = broker.resolve("thematicResource", String.format(path, row, accountId, domainKey, requestId, url, fl, sc2Mode, requestType, theme, debugMode));
+     Resource thematic = broker.resolve("thematicResource", String.format(path, row, accountId, domainKey, requestId, url, fl, sc2Mode, feRequestType, theme, debugMode));
      int items = (int) thematic.getValue("response/numFound");
      if (thematic != null && items != 0) {
        request.setAttribute("thematic", thematic);

96b7a01fb7fc65dad34c9bf4ca8b438f11db105a
Varun <varun.wachaspati@bloomreach.com>
2018-10-29 14:09:32 +0100

    bug fix for Pagination

--- plugins/thematic-plugin/src/main/java/com/dxpfc/thematic/components/ThematicBaseComponent.java
+++ plugins/thematic-plugin/src/main/java/com/dxpfc/thematic/components/ThematicBaseComponent.java
@@ -1,5 +1,7 @@
 package com.dxpfc.thematic.components;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.onehippo.cms7.crisp.core.resource.jackson.JacksonResource;
 import org.onehippo.cms7.essentials.components.CommonComponent;
 import org.hippoecm.hst.core.component.HstComponentException;
 import org.hippoecm.hst.core.component.HstRequest;
@@ -13,6 +15,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.client.ResourceAccessException;
 
+import java.io.IOException;
 import java.util.Map;
 import java.util.UUID;
 
@@ -39,11 +42,11 @@ public class ThematicBaseComponent extends CommonComponent {
     Map<String,String> properties = getComponentParameters();
 
     String sort = params.getOrDefault(ThematicConstants.SORT_ORDER, new String[]{""})[0];
-    int page = new Integer(params.getOrDefault(ThematicConstants.PAGINATION, new String[]{"0"})[0]);
+    int page = new Integer(params.getOrDefault(ThematicConstants.PAGINATION, new String[]{"1"})[0]);
 
     String fl = properties.get(ThematicConstants.PROPERTIES_FIELDS);
     String resultsPerPage = properties.get(ThematicConstants.PROPERTIES_MAX_RESULTS_PER_PAGE);
-    String start = Integer.toString(page * Integer.parseInt(resultsPerPage));
+    String start = Integer.toString((page-1) * Integer.parseInt(resultsPerPage));
     String accountId = properties.get(ThematicConstants.PROPERTIES_ACCOUNT_ID);
     String domainKey = properties.get(ThematicConstants.PROPERTIES_DOMAIN_KEY);
     String sc2Mode = properties.get(ThematicConstants.PROPERTIES_SC2_MODE);
@@ -55,22 +58,21 @@ public class ThematicBaseComponent extends CommonComponent {
     try {
       Resource thematic = broker.resolve("thematicResource", String.format(path, resultsPerPage, accountId, domainKey, requestId, url, fl, sc2Mode, feRequestType, themeName, sort, start, debugMode));
 
-      long items = (long) thematic.getValue("response/numFound");
+      int items = (int) thematic.getValue("response/numFound");
+      String leftNav = (String)thematic.getValue("page_header/left_nav");
       if (thematic != null && items != 0) {
         request.setAttribute("thematic", thematic);
         request.setAttribute("sort",sort);
         request.setAttribute("currentPage",page);
         request.setAttribute("totalPages",1 + ((items-1)/Integer.parseInt(resultsPerPage)));
+        request.setAttribute("leftNav",leftNav.isEmpty() ? "" : new JacksonResource(new ObjectMapper().readTree(leftNav)));
       }else
         super.pageNotFound(response);
-    } catch (ResourceException | ResourceAccessException e) {
+    } catch (ResourceException | ResourceAccessException | IOException e) {
       log.error("Unable to resolve Thematic resource - ",e);
       super.pageNotFound(response);
     }
 
   }
-
-
-
 
 }

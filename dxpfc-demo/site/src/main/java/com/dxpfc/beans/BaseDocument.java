803d0dd7593ab067c67eeaabf954749f5ee5002e
Giacomo Lamonaco <giacomo.lamonaco@bloomreach.com>
2019-02-01 15:21:05 +0100

    DXPFORC-157: Refactoring folder structure

--- dxpfc-demo/site/src/main/java/com/dxpfc/beans/BaseDocument.java
+++
@@ -1,9 +0,0 @@
-package com.dxpfc.beans;
-
-import org.hippoecm.hst.content.beans.Node;
-import org.hippoecm.hst.content.beans.standard.HippoDocument;
-
-@Node(jcrType="dxpfcdemo:basedocument")
-public class BaseDocument extends HippoDocument {
-
-}
